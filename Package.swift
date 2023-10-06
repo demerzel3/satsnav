810af7a4c79b1c3d5d856bd778a4ee22a0115aed
Gabriele Genta <gabriele.genta@gmail.com>
2023-10-06 22:59:03 +0200

    First commit

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SatsNav",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/lukepistrol/KrakenAPI.git", .upToNextMajor(from: "2.1.0")),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .executableTarget(
+            name: "SatsNav", dependencies: ["KrakenAPI"]
+        ),
+    ]
+)
