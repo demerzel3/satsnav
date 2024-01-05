4eb8b394ad0b525e1c39e3dce0abc58114ec36cb
Gabriele Genta <gabriele.genta@gmail.com>
2024-01-05 22:03:09 +0100

    Add synching of onchain wallet on app open

--- swift-ui/SatsNav/OnChainWallet.swift
+++ swift-ui/SatsNav/OnChainWallet.swift
@@ -3,11 +3,26 @@ import RealmSwift
 
 final class OnchainWallet: Object {
     @Persisted(primaryKey: true) var name: String
-    @Persisted var addresses: List<Address>
+    @Persisted var addresses: List<OnchainWalletAddress>
 
-    convenience init(name: String, addresses: List<Address>) {
+    convenience init(name: String, addresses: List<OnchainWalletAddress>) {
         self.init()
         self.name = name
         self.addresses = addresses
+    }
+}
+
+final class OnchainWalletAddress: EmbeddedObject {
+    @Persisted var id: String
+    @Persisted var scriptHash: String
+
+    convenience init(id: String, scriptHash: String) {
+        self.init()
+        self.id = id
+        self.scriptHash = scriptHash
+    }
+
+    func toAddress() -> Address {
+        Address(id: id, scriptHash: scriptHash)
     }
 }
