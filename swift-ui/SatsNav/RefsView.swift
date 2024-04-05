ae0b1f795259e57502dcb1134855019e42ebef12
Gabriele Genta <gabriele.genta@gmail.com>
2024-04-05 22:50:41 +0200

    Remove nested navigator

--- swift-ui/SatsNav/RefsView.swift
+++ swift-ui/SatsNav/RefsView.swift
@@ -12,25 +12,23 @@ struct RefsView: View {
     let dateFormatter: DateFormatter = createDateFormatter()
 
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(refs.reversed()) { item in
-                    NavigationLink(destination: { RefIdsView(refIds: item.refIds) }) {
-                        HStack {
-                            VStack(alignment: .leading) {
-                                Text("\(formatBtcAmount(item.amount)) BTC")
-                                if let rate = item.rate {
-                                    Text("\(formatFiatAmount(rate)) EUR")
-                                } else {
-                                    Text("No rate")
-                                }
+        List {
+            ForEach(refs.reversed()) { item in
+                NavigationLink(destination: { RefsView(refs: item.spends) }) {
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text("\(formatBtcAmount(item.amount)) BTC")
+                            if let rate = item.rate {
+                                Text("\(formatFiatAmount(rate)) EUR")
+                            } else {
+                                Text("No rate")
                             }
-                            Spacer()
-                            Text("\(dateFormatter.string(from: item.date))")
                         }
+                        Spacer()
+                        Text("\(dateFormatter.string(from: item.date))")
                     }
                 }
-            }.listStyle(.plain)
-        }
+            }
+        }.listStyle(.plain)
     }
 }
