b654c64a85beea08007f5942ed87a69b98db3f9b
Gabriele Genta <gabriele.genta@gmail.com>
2024-08-31 00:07:45 +0200

    Iteratively look for holes and fix them

--- graph/src/collapseBaseCurrencyConversions.ts
+++ graph/src/collapseBaseCurrencyConversions.ts
@@ -30,109 +30,120 @@ export function collapseBaseCurrencyConversions(graph: BalanceGraph) {
 
     console.log("Conversion to EUR", nodeIdToString(anchorNodeId));
 
-    // Trace everything related to this conversion back to the last time this was in EUR
-    // console.log(
-    //     `Conversion to EUR - ${refToString(
-    //         refChange.convert.toRef
-    //     )}`
-    // );
-
     const visitedNodes: Set<string> = new Set([anchorNodeId]);
     const nodesToVisit: string[] = g.inNeighbors(anchorNodeId);
     const nodesToVisitSet: Set<string> = new Set(nodesToVisit);
 
-    while (nodesToVisit.length > 0) {
-        const nodeId = nodesToVisit.pop()!;
-        nodesToVisitSet.delete(nodeId);
-        const node = graph.getNode(nodeId);
+    const visit = () => {
+        while (nodesToVisit.length > 0) {
+            const nodeId = nodesToVisit.pop()!;
+            nodesToVisitSet.delete(nodeId);
+            const node = graph.getNode(nodeId);
 
-        // TODO: revisit this to keep track of withdrawals, single trades and such
-        if (!("ref" in node)) {
-            continue;
-        }
+            // TODO: revisit this to keep track of withdrawals, single trades and such
+            if (!("ref" in node)) {
+                continue;
+            }
 
-        visitedNodes.add(nodeId);
+            visitedNodes.add(nodeId);
 
-        // Stop iteration on EUR nodes with no visited parents
-        if (
-            node.ref.asset.name === "EUR" &&
-            (g.inDegree(nodeId) === 1 || !g.someInNeighbor(nodeId, (inNodeId) => visitedNodes.has(inNodeId)))
-        ) {
-            // console.log("Found EUR", nodeToString(node));
-            // sourceNodes.add(nodeId);
-            continue;
-        }
+            // Stop iteration on EUR nodes with no visited parents
+            if (
+                node.ref.asset.name === "EUR" &&
+                (g.inDegree(nodeId) === 1 || !g.someInNeighbor(nodeId, (inNodeId) => visitedNodes.has(inNodeId)))
+            ) {
+                // console.log("Found EUR", nodeToString(node));
+                // sourceNodes.add(nodeId);
+                continue;
+            }
 
-        // TODO: if outDegree is 0 and not EUR and with rate we should fail
-        if (node.ref.asset.name !== "EUR" || g.outDegree(nodeId) > 1) {
-            g.outNeighbors(nodeId).forEach((outNodeId) => {
-                if (visitedNodes.has(outNodeId) || nodesToVisitSet.has(outNodeId)) {
+            // TODO: if outDegree is 0 and not EUR and with rate we should fail
+            if (node.ref.asset.name !== "EUR" || g.outDegree(nodeId) > 1) {
+                g.outNeighbors(nodeId).forEach((outNodeId) => {
+                    if (visitedNodes.has(outNodeId) || nodesToVisitSet.has(outNodeId)) {
+                        return;
+                    }
+
+                    nodesToVisit.push(outNodeId);
+                    nodesToVisitSet.add(outNodeId);
+                });
+            }
+
+            const parentIds = g.inNeighbors(nodeId);
+            // No more parents to look at and this is not EUR, failed to combine nodes
+            // if (parentIds.length === 0) {
+            //     // Make sure nodesToVisit is not empty as this is our failure check
+            //     nodesToVisit.push(nodeId);
+            //     nodesToVisitSet.add(nodeId);
+            //     break;
+            // }
+
+            parentIds.forEach((parentNodeId) => {
+                if (visitedNodes.has(parentNodeId) || nodesToVisitSet.has(parentNodeId)) {
                     return;
                 }
 
-                nodesToVisit.push(outNodeId);
-                nodesToVisitSet.add(outNodeId);
+                nodesToVisit.push(parentNodeId);
+                nodesToVisitSet.add(parentNodeId);
             });
+
+            if (nodesToVisit.length !== nodesToVisitSet.size) {
+                console.log(nodesToVisit);
+                console.log([...nodesToVisitSet]);
+                throw new Error("Male male");
+            }
         }
+    };
 
-        const parentIds = g.inNeighbors(nodeId);
-        // No more parents to look at and this is not EUR, failed to combine nodes
-        // if (parentIds.length === 0) {
-        //     // Make sure nodesToVisit is not empty as this is our failure check
-        //     nodesToVisit.push(nodeId);
-        //     nodesToVisitSet.add(nodeId);
-        //     break;
-        // }
-
-        parentIds.forEach((parentNodeId) => {
-            if (visitedNodes.has(parentNodeId) || nodesToVisitSet.has(parentNodeId)) {
+    const getEdgeNodes = (): [sourceNodes: Set<string>, terminalNodes: Set<string>] => {
+        const sourceNodes: Set<string> = new Set();
+        const terminalNodes: Set<string> = new Set();
+        visitedNodes.forEach((nodeId) => {
+            // No inbound nodes at all, or no visited ones
+            if (!g.someInNeighbor(nodeId, (inNodeId) => visitedNodes.has(inNodeId))) {
+                sourceNodes.add(nodeId);
                 return;
             }
-
-            nodesToVisit.push(parentNodeId);
-            nodesToVisitSet.add(parentNodeId);
+            // No outbound nodes at all, or no visited ones
+            if (!g.someOutNeighbor(nodeId, (outNodeId) => visitedNodes.has(outNodeId))) {
+                terminalNodes.add(nodeId);
+                return;
+            }
         });
 
-        if (nodesToVisit.length !== nodesToVisitSet.size) {
-            console.log(nodesToVisit);
-            console.log([...nodesToVisitSet]);
-            throw new Error("Male male");
-        }
-    }
+        return [sourceNodes, terminalNodes];
+    };
 
-    // Process terminated early, was not possible to simplify
-    if (nodesToVisit.length > 0) {
-        return;
-    }
-
-    const sourceNodes: Set<string> = new Set();
-    const terminalNodes: Set<string> = new Set();
-    visitedNodes.forEach((nodeId) => {
-        // No inbound nodes at all, or no visited ones
-        if (!g.someInNeighbor(nodeId, (inNodeId) => visitedNodes.has(inNodeId))) {
-            sourceNodes.add(nodeId);
+    while (nodesToVisit.length > 0) {
+        visit();
+        // Process terminated early, was not possible to simplify
+        if (nodesToVisit.length > 0) {
             return;
         }
-        // No outbound nodes at all, or no visited ones
-        if (!g.someOutNeighbor(nodeId, (outNodeId) => visitedNodes.has(outNodeId))) {
-            terminalNodes.add(nodeId);
-            return;
-        }
-    });
 
-    let path: string[] | null = null;
-    for (const terminalNodeId of terminalNodes) {
-        for (const sourceNodeId of sourceNodes) {
-            path = bidirectional(g, terminalNodeId, sourceNodeId);
+        const [sourceNodes, terminalNodes] = getEdgeNodes();
+        let path: string[] | null = null;
+        for (const terminalNodeId of terminalNodes) {
+            for (const sourceNodeId of sourceNodes) {
+                path = bidirectional(g, terminalNodeId, sourceNodeId);
+                if (path) break;
+            }
             if (path) break;
         }
-        if (path) break;
+
+        if (path) {
+            console.log("Path found!", path.map(nodeIdToString));
+
+            path.forEach((pathNodeId) => {
+                if (visitedNodes.has(pathNodeId)) return;
+
+                nodesToVisit.push(pathNodeId);
+                nodesToVisitSet.add(pathNodeId);
+            });
+        }
     }
 
-    if (path) {
-        console.log("Path found!", path.map(nodeIdToString));
-    }
-
+    const [sourceNodes, terminalNodes] = getEdgeNodes();
     console.log("SUCCESS", visitedNodes.size, "visited", sourceNodes.size, "sources", terminalNodes.size, "terminals");
     sourceNodes.forEach((nodeId) => {
         console.log("s---", nodeIdToString(nodeId));
